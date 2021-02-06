4bd6e26b4791bb54422610ae5890beb920fc10ce
HARYADIN666 <78267856+HARYADIN666@users.noreply.github.com>
2021-02-07 06:46:03 +0700

    Update download.js

--- database/menu/download.js
+++ database/menu/download.js
@@ -1,6 +1,6 @@
 const download = (pushname, prefix, botName, ownerName, getLevelingLevel, sender, _registered) => {
 	return `Hallo, ${pushname}
-ꜱɪʟᴀʜᴋᴀʜ ᴩɪʟɪʜ\nᴍᴇɴᴜ ᴅɪʙᴀᴡᴀʜ
+ꜱɪʟᴀʜᴋᴀʜ ᴩɪʟɪʜ\nᴍᴇɴᴜ ᴅɪʙᴀᴡᴀʜ...
 ╭──────────────
 │ 《✰ᴅᴏᴡɴʟᴏᴀᴅ✰》
 ╰──────────────
