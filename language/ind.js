8c1c990609d1f802b86faadc330a19654d2146ee
HARYADIN666 <78267856+HARYADIN666@users.noreply.github.com>
2021-02-07 05:56:02 +0700

    Add files via upload

---
+++ language/ind.js
@@ -0,0 +1,138 @@
+exports.wait = () => {
+	return`*〘ᴡᴀɪᴛ ꜱᴇᴅᴀɴɢ ᴩʀᴏꜱᴇꜱ〙*`
+}
+
+exports.succes = () => {
+	return`*〖Sｕｋｓｅｓ〗*`
+}
+
+exports.lvlon = () => {
+	return`*〘ᴇɴᴀʙʟᴇ ʟᴇᴠᴇʟɪɴɢ〙*`
+}
+
+exports.lvloff = () => {
+	return`*〘ᴅɪꜱᴀʙʟᴇ ʟᴇᴠᴇʟɪɴɢ〙*`
+}
+
+exports.lvlnul = () => {
+	return`〘мᴀᴀғ ʟᴇᴠᴇʟᴍᴜ ᴍᴀꜱɪʜ ᴋᴏꜱᴏɴɢ〙`
+}
+
+exports.lvlnoon = () => {
+	return`*〘ʟᴇᴠᴇʟ ᴅɪ ɢʀᴜᴩ ʙᴇʟᴜᴍ ᴅɪ ᴀᴋᴛɪꜰᴋᴀɴ〙*`
+}
+
+exports.noregis = () => {
+	return`*〘ᴀɴᴅᴀ ʙᴇʟᴜᴍ ᴛᴇʀᴅᴀꜰᴛᴀʀ〙*\n*ᴄᴀʀᴀ ᴅᴀꜰᴛᴀʀ ${prefix}ᴅᴀꜰᴛᴀʀ ɴᴀᴍᴀ|ᴜᴍᴜʀ* \n\n*ᴄᴏɴᴛᴏʜ ${prefix}ᴅᴀꜰᴛᴀʀ ʜᴀʀyᴀᴅɪɴ|17*`
+}
+
+exports.baned = () => {
+	return`*ꜱᴏʀʀy ᴀᴊᴀ ɴɪʜ ʙʀᴏ, ᴛᴀᴩɪ ᴋᴀᴜ ꜱᴜᴅᴀʜ ᴋᴜ ʙᴀɴɴᴇᴅ Yᴀʜᴀʜᴀʜᴀ ʜᴀyᴜᴜᴜ :ᴠ*`
+}
+
+exports.rediregis = () => {
+	return`*〖 ꜱᴜᴅᴀʜ ᴛᴇʀᴅᴀꜰᴛᴀʀ 〗*\n\n*ᴋᴀᴍᴜ ꜱᴜᴅᴀʜ ᴛᴇʀᴅᴀꜰᴛᴀʀ ᴅɪ ᴅᴀᴛᴀʙᴀꜱᴇ ʙᴏᴛ*`
+}
+
+exports.stikga = () => {
+	return`*yᴀʜ ɢᴀɢᴀʟ ᴄᴏʙᴀ ᴜʟᴀɴɢɪ ʙᴇʙᴇʀᴀᴩᴀ ꜱᴀᴀᴛ ʟᴀɢɪ...*`
+}
+
+exports.linkga = () => {
+	return`*maaf link tidak valid*`
+}
+
+exports.groupo = () => {
+	return`*〘ʜᴀɴyᴀ ʙɪꜱᴀ ᴅɪɢᴜɴᴀᴋᴀɴ ɢʀᴏᴜᴩ〙*`
+}
+
+exports.ownerb = () => {
+	return`*ʜᴀɴyᴀ ʙɪꜱᴀ ᴅɪɢᴜɴᴀᴋᴀɴ ᴏʟᴇʜ ᴏᴡɴᴇʀ*`
+}
+
+exports.ownerg = () => {
+	return`*〘ʜᴀɴyᴀ ʙɪꜱᴀ ᴅɪɢᴜɴᴀᴋᴀɴ ᴏʟᴇʜ ᴏᴡɴᴇʀ ɢʀᴏᴜᴩ〙*`
+}
+
+exports.admin = () => {
+	return`*〘ᴀᴅᴍɪɴ ɢʀᴏᴜᴩ ᴏɴʟy〙*`
+}
+
+exports.badmin = () => {
+	return`*〘ʙᴏᴛ ʜᴀʀᴜꜱ ᴊᴀᴅɪ ᴀᴅᴍɪɴ〙*`
+}
+
+exports.nsfwoff = () => {
+	return`*〘ɴꜱꜰᴡ ɢᴀᴋ ᴀᴋᴛɪꜰ〙*`
+}
+
+exports.bug = () => {
+	return`*ᴍᴀꜱᴀʟᴀʜ ᴛᴇʟᴀʜ ᴅɪ ʟᴀᴩᴏʀᴋᴀɴ ᴋᴇ ᴏᴡɴᴇʀ ʙᴏᴛ, ʟᴀᴩᴏʀᴀɴ ᴩᴀʟꜱᴜ/ᴍᴀɪɴ2 ᴛɪᴅᴀᴋ ᴀᴋᴀɴ ᴅɪᴛᴀɴɢɢᴀᴩɪ*`
+}
+
+exports.wrongf = () => {
+	return`*〘ᴛᴇᴋꜱ ɴyᴀ ᴍᴀɴᴀ ᴋᴀᴋ?〙*`
+}
+
+exports.clears = () => {
+	return`*〖ᴄʟᴇᴀʀ ᴀʟʟ ꜱᴜᴄᴄᴇꜱꜱ〗*`
+}
+
+exports.pc = () => {
+	return`*〖ʀᴇɢɪꜱᴛʀᴀꜱɪ〗*\n\ɴᴜɴᴛᴜᴋ ᴍᴇɴɢᴇᴛᴀʜᴜɪ ᴀᴩᴀ ᴋᴀᴍᴜ ꜱᴜᴅᴀʜ ᴛᴇʀᴅᴀꜰᴛᴀʀ ꜱɪʟᴀʜᴋᴀʜ ᴄʜᴇᴄᴋ ᴍᴇꜱꜱᴀɢᴇ yᴀɴɢ ꜱᴀyᴀ ᴋɪʀɪᴍ \n\nNOTE:\n*ᴊɪᴋᴀ ᴋᴀᴍᴜ ʙᴇʟᴜᴍ ᴍᴇɴᴅᴀᴩᴀᴛᴋᴀɴ ᴩᴇꜱᴀɴ. ʙᴇʀᴀʀᴛɪ ᴋᴀᴍᴜ ʙᴇʟᴜᴍ ᴍᴇɴyɪᴍᴩᴀɴ ɴᴏᴍᴇʀ ʙᴏᴛ*`
+}
+
+exports.registered = (namaUser, umurUser, serialUser, time, sender, botName) => {
+	return`*〖ʀᴇɢɪꜱᴛʀᴀꜱɪ ʙᴇʀʜᴀꜱɪʟ〗*\nᴜɴᴛᴜᴋ ᴜꜱᴇʀ ɪɴꜰᴏ: \n\n*➥ ɴᴀᴍᴀ : ${namaUser}*\n*➥ Nomor : wa.me/${sender.split("@")[0]}*\n*➥ Umur : ${umurUser}*\n*➥ ᴡᴀᴋᴛᴜ ᴅᴀꜰᴛᴀʀ : ${time}*\n\n*[ SN ] : ${serialUser}*\nɴᴏᴛᴇ : ᴊᴀɴɢᴀɴ ʜᴀᴩᴜꜱ ᴄʜᴀᴛ ɪɴɪ ꜱɪᴀᴩᴀ ᴛᴀᴜ ʙᴜᴛᴜʜ :ᴠ`
+}
+
+exports.cmdnf = (prefix, command) => {
+	return`command *${prefix}${command}* *ᴛɪᴅᴀᴋ ᴅɪᴇᴍᴜᴋᴀɴ\ᴄᴏʙᴀ ᴛᴜʟɪꜱ* *${prefix}ᴍᴇɴᴜ*`
+}
+
+exports.owneresce = (pushname) => {
+	return`*ᴍᴀᴀꜰ ᴛᴀᴩɪ ${pushname} ʙᴜᴋᴀɴ ᴏᴡɴᴇʀ ꜱᴄʀɪᴩᴛ*`
+}
+
+exports.reglevelaha = (command, pushname, getLevelingLevel, sender, aha) => {
+	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏➸level mu : ${getLevelingLevel(sender)}*\n*┣➸jenis command : ${command}*\n*┗➸syarat level : ${aha}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+
+exports.reglevelahb = (command, pushname, getLevelingLevel, sender, ahb) => {
+	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏➸level mu : ${getLevelingLevel(sender)}*\n*┣➸jenis command : ${command}*\n*┗➸syarat level : ${ahb}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+
+exports.reglevelahc = (command, pushname, getLevelingLevel, sender, ahc) => {
+	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏➸level mu : ${getLevelingLevel(sender)}*\n*┣➸jenis command : ${command}*\n*┗➸syarat level : ${ahc}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+
+exports.reglevelahd = (command, pushname, getLevelingLevel, sender, ahd) => {
+	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏➸level mu : ${getLevelingLevel(sender)}*\n*┣➸jenis command : ${command}*\n*┗➸syarat level : ${ahd}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+
+exports.reglevelahe = (command, pushname, getLevelingLevel, sender, ahe) => {
+	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏➸level mu : ${getLevelingLevel(sender)}*\n*┣➸jenis command : ${command}*\n*┗➸syarat level : ${ahe}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+
+exports.reglevelahf = (command, pushname, getLevelingLevel, sender, ahf) => {
+	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏➸level mu : ${getLevelingLevel(sender)}*\n*┣➸jenis command : ${command}*\n*┗➸syarat level : ${ahf}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+exports.levelup = (pushname, sender, getLevelingXp,  getLevel, getLevelingLevel) => {
+	return`
+╭───────────────\n│        〘 ꜱᴇʟᴀᴍᴀᴛ〙           \n╰───────────────\n╭───────────────\n│➥ *Nama* : ${pushname}\n│➥ *Nomor* : wa.me/${sender.split("@")[0]}\n│➥ *Xp* : ${getLevelingXp(sender)}\n│➥ *Level* : ${getLevel} ➪ ${getLevelingLevel(sender)}\n╰───────────────
+`}
+ 
+exports.limitend = (pushname) => {
+	return`*ᴍᴀᴀꜰ ${pushname} ʟɪᴍɪᴛ ʜᴀʀɪ ɪɴɪ ʜᴀʙɪꜱ*\n*ʟɪᴍɪᴛ ᴅɪ ʀᴇꜱᴇᴛ ꜱᴇᴛɪᴀᴩ ᴊᴀᴍ 24:00*`
+}
+
+exports.limitcount = (limitCounts) => {
+	return`╭───────────────\n│         〘 ʟɪᴍɪᴛ ᴀᴄᴄᴏᴜɴᴛ 〙           \n╰───────────────\n╭───────────────\n│ʟɪᴍɪᴛ ᴀɴᴅᴀ : ${limitCounts}\n╰───────────────\n*ᵁᴾᴳᴿᴬᴰᴱ ᴾᴿᴱᴹᴵᵁᴹ ᴶᴵᴷᴬ ᴵᴺᴳᴵᴺ*\n*ʙᴇʙᴀꜱ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ʙᴏᴛ*`
+}
+exports.satukos = () => {
+	return`*ᵀᵃᵐᵇᵃʰ ᵖᵃʳᵃᵐᵉᵗᵉʳ ¹/ᵉⁿᵃᵇˡᵉ ᵃᵗᵃᵘ ⁰/ᵈⁱˢᵃᵇˡᵉ`
+}
+
+exports.uangkau = (pushname, sender, uangkau) => {
+	return`╭───────────────\n│             🇦 🇹 🇲                \n╰───────────────\n╭───────────────\n│➥ ɴᴀᴍᴀ : ${pushname}\n│➥ ɴᴏᴍᴏʀ : ${sender.split("@")[0]}\n│➥ ᴜᴀɴɢ : ${uangkau}\n╰───────────────`
+}
